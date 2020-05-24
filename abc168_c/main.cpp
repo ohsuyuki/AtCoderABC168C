946021ce7cad3945fd33ae173eca9b81d2a962c4
ohsuyuki <papu8osu@gmail.com>
2020-05-24 22:27:35 +0900

    Initial Commit

---
+++ abc168_c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  abc168_c
+//
+//  Created by yuuki oosu on 2020/05/24.
+//  Copyright © 2020 yuuki oosu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
